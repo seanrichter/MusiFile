9ab34877c546063d2db4591d96504cc9ab49b597
Sean Richter <sean.richter04@outlook.com>
2019-11-13 23:09:46 -0600

    was able to append all data from lastfm api to the page

--- models/schema.sql
+++ models/schema.sql
@@ -1,64 +1,64 @@
-DROP SCHEMA IF EXISTS musifile_db;
+-- DROP SCHEMA IF EXISTS musifile_db;
 
-CREATE SCHEMA musifile_db;
-USE musifile_db;
+-- CREATE SCHEMA musifile_db;
+-- USE musifile_db;
 
-CREATE TABLE artists (                                                     
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(40) NOT NULL,
-    PRIMARY KEY(id),
-    UNIQUE KEY(name)
-);
+-- CREATE TABLE artists (                                                     
+--     id int NOT NULL AUTO_INCREMENT,
+--     name varchar(40) NOT NULL,
+--     PRIMARY KEY(id),
+--     UNIQUE KEY(name)
+-- );
 
-CREATE TABLE albums (
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(40) NOT NULL,
-    artist int NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(artist) REFERENCES artists(id)
-);
+-- CREATE TABLE albums (
+--     id int NOT NULL AUTO_INCREMENT,
+--     name varchar(40) NOT NULL,
+--     artist int NOT NULL,
+--     PRIMARY KEY(id),
+--     FOREIGN KEY(artist) REFERENCES artists(id)
+-- );
 
-CREATE TABLE tracks (
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(40) NOT NULL,
-    album int NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(album) REFERENCES albums(id)
-);
+-- CREATE TABLE tracks (
+--     id int NOT NULL AUTO_INCREMENT,
+--     name varchar(40) NOT NULL,
+--     album int NOT NULL,
+--     PRIMARY KEY(id),
+--     FOREIGN KEY(album) REFERENCES albums(id)
+-- );
 
-INSERT INTO artists (name) VALUES ('Ellie Goulding');
-INSERT INTO artists (name) VALUES ('Florence + The Machine');
-INSERT INTO artists (name) VALUES ('Ed Sheeran');
+-- INSERT INTO artists (name) VALUES ('Ellie Goulding');
+-- INSERT INTO artists (name) VALUES ('Florence + The Machine');
+-- INSERT INTO artists (name) VALUES ('Ed Sheeran');
 
-INSERT INTO albums (name, artist) VALUES ('Lights', 1);
-INSERT INTO albums (name, artist) VALUES ('Lungs', 2);
-INSERT INTO albums (name, artist) VALUES ('x(Deluxe Edition)', 3);
+-- INSERT INTO albums (name, artist) VALUES ('Lights', 1);
+-- INSERT INTO albums (name, artist) VALUES ('Lungs', 2);
+-- INSERT INTO albums (name, artist) VALUES ('x(Deluxe Edition)', 3);
 
-SELECT albums.id AS 'Album ID',
-    albums.name AS 'Album Name',
-    artists.name AS 'Artist Name'
-    FROM albums
-    JOIN artists ON (artists.id = albums.artist);
+-- SELECT albums.id AS 'Album ID',
+--     albums.name AS 'Album Name',
+--     artists.name AS 'Artist Name'
+--     FROM albums
+--     JOIN artists ON (artists.id = albums.artist);
     
-INSERT INTO tracks (name, album) VALUES ('Bloodstream', 3);
-INSERT INTO tracks (name, album) VALUES ("Don't", 3);
+-- INSERT INTO tracks (name, album) VALUES ('Bloodstream', 3);
+-- INSERT INTO tracks (name, album) VALUES ("Don't", 3);
 
-INSERT INTO tracks (name, album) VALUES ('Drumming track', 2);
-INSERT INTO tracks (name, album) VALUES ('Blinding', 2);
+-- INSERT INTO tracks (name, album) VALUES ('Drumming track', 2);
+-- INSERT INTO tracks (name, album) VALUES ('Blinding', 2);
 
-INSERT INTO tracks (name, album) VALUES ('Lights - Single Version', 1);
-INSERT INTO tracks (name, album) VALUES ('Starry Eyed', 1);
+-- INSERT INTO tracks (name, album) VALUES ('Lights - Single Version', 1);
+-- INSERT INTO tracks (name, album) VALUES ('Starry Eyed', 1);
 
-SELECT name FROM tracks;
+-- SELECT name FROM tracks;
 
-SELECT tracks.name AS 'Track Name',
-    albums.name AS 'Album Name'
-    FROM tracks
-    JOIN albums ON (albums.id = tracks.album);
+-- SELECT tracks.name AS 'Track Name',
+--     albums.name AS 'Album Name'
+--     FROM tracks
+--     JOIN albums ON (albums.id = tracks.album);
     
-SELECT tracks.name AS 'Track Name',
-    albums.name AS 'Album Name',
-    artists.name AS 'Artist Name'
-    FROM tracks
-    JOIN albums ON (albums.id = tracks.album)
-    JOIN artists ON (artists.id = albums.artist);    
+-- SELECT tracks.name AS 'Track Name',
+--     albums.name AS 'Album Name',
+--     artists.name AS 'Artist Name'
+--     FROM tracks
+--     JOIN albums ON (albums.id = tracks.album)
+--     JOIN artists ON (artists.id = albums.artist);    
