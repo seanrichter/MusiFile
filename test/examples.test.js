101f713a60bc813613047551ec31ac35cbbd7dfb
Sean Richter <sean.richter04@outlook.com>
2019-11-13 20:08:51 -0600

    trying to loop through the array of objects

--- test/examples.test.js
+++ test/examples.test.js
@@ -13,29 +13,3 @@ axios
     // Then we print out the imdbRating
     console.log("The movie's rating is: " + response.data.imdbRating);
   });
-
-//test that chaz ran in class to single down results
-var artistMatches = results.artistmatches.artist;
-//console.log(artistMatches);
-var artistAbridged = artistMatches
-  .map(function(item) {
-    try {
-      return {
-        name: item.name,
-        url: item.url,
-        //image: item.image[0] 
-        // eslint-disable-next-line indent
-        image: item.image
-      };
-    } catch (err) {
-      // don't do anything
-    }
-  }
-
-  .filter(function(item) {
-    return (item.name !== undefined && 
-    item.image !==undefined &&
-    item.url !== undefined)
-  });
-
-console.log(artistAbridged);
